578ae14091b5de1dae0b880fe10a23eab25194af
h-kaname <luis--ao@hotmail.com>
2021-09-12 05:10:21 -0300

    added slideBanner-footer & style responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+
+console.log('Test js');
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.container', {delay: 500});
+ScrollReveal().reveal('.container', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
